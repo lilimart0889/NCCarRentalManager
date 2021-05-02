fc6ee3111a97f45a77d1fb30d7a37dc1cd1b69a8
Lili <79122251+lilimart0889@users.noreply.github.com>
2021-05-02 18:48:17 -0500

    SQL CODE
    1. Script for creating tables.
    2. Script for insert vehicles into vehicles table.
    3. Script for insert gamma into gamma_vehicles table.

---
+++ SISTEMA_RENTACAR.sql
@@ -0,0 +1,107 @@
+-- MySQL Workbench Forward Engineering
+-- AUTOR: LILI MP
+-- NC TRAINEE PROGRAM
+-- FINAL PROYECT: RENT A CAR "TRIPS BY WHEELS"
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema CarRental
+-- -----------------------------------------------------
+-- Modelo de administracion de usuarios para renta de coches
+DROP SCHEMA IF EXISTS `sistema` ;
+
+-- -----------------------------------------------------
+-- Schema CarRental
+--
+-- Modelo de administracion de usuarios para renta de coches
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `sistema` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+USE `sistema` ;
+
+-- -----------------------------------------------------
+-- Table `sistema`.`USERS_GUEST`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sistema`.`USERS_GUEST` ;
+
+CREATE TABLE IF NOT EXISTS `sistema`.`USERS_GUEST` (
+  `ID_USER_GUEST` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` VARCHAR(60) NOT NULL,
+  `LAST_NAME` VARCHAR(60) NOT NULL,
+  `AGE` INT NOT NULL,
+  `RESIDENCE` VARCHAR(60) NOT NULL,
+  `PHONE_NUMBER` INT UNSIGNED NOT NULL,
+  `EMAIL` VARCHAR(320) NOT NULL,
+  PRIMARY KEY (`ID_USER_GUEST`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sistema`.`GAMMA_VEHICLES`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sistema`.`GAMMA_VEHICLES` ;
+
+CREATE TABLE IF NOT EXISTS `sistema`.`GAMMA_VEHICLES` (
+  `ID_GAMA_VEHICLES` VARCHAR(45) NOT NULL,
+  `DESCRIPTION` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`ID_GAMA_VEHICLES`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sistema`.`VEHICLES`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sistema`.`VEHICLES` ;
+
+CREATE TABLE IF NOT EXISTS `sistema`.`VEHICLES` (
+  `NUMBER_PLATE` INT NOT NULL AUTO_INCREMENT,
+  `BRAND` VARCHAR(45) NULL,
+  `ID_GAMA_VEHICLES` VARCHAR(45) NOT NULL,
+  `MODEL` VARCHAR(45) NULL,
+  `TRANSMISSION` VARCHAR(45) NULL,
+  `ISAVAILABILITY` TINYINT(1) NOT NULL,
+  `SEATS_CAPACITY` INT NULL,
+  `PRICE` FLOAT NOT NULL,
+  PRIMARY KEY (`NUMBER_PLATE`, `ID_GAMA_VEHICLES`),
+  INDEX `GAMMA_VEHICLES1_IDX` (`ID_GAMA_VEHICLES` ASC) VISIBLE,
+  CONSTRAINT `GAMMA_VEHICLES_FK`
+    FOREIGN KEY (`ID_GAMA_VEHICLES`)
+    REFERENCES `sistema`.`GAMMA_VEHICLES` (`ID_GAMA_VEHICLES`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sistema`.`RESERVATIONS`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `sistema`.`RESERVATIONS` ;
+
+CREATE TABLE IF NOT EXISTS `sistema`.`RESERVATIONS` (
+  `ID_RESERVATION` INT NOT NULL AUTO_INCREMENT,
+  `PICKUP_DATE` DATE NOT NULL,
+  `RETURN_DATE` DATE NOT NULL,
+  `TOTAL_PRICE` DOUBLE NOT NULL,
+  `ID_USER_GUEST` INT UNSIGNED NOT NULL,
+  `VEHICLES_NUMBER_PLATE` INT NOT NULL,
+  PRIMARY KEY (`ID_RESERVATION`, `ID_USER_GUEST`, `VEHICLES_NUMBER_PLATE`),
+  INDEX `ID_USER_GUEST_FK` (`ID_USER_GUEST` ASC) VISIBLE,
+  INDEX `NUMBER_PLATE` (`VEHICLES_NUMBER_PLATE` ASC) VISIBLE,
+  CONSTRAINT `ID_USER_GUEST_FK`
+    FOREIGN KEY (`ID_USER_GUEST`)
+    REFERENCES `sistema`.`USERS_GUEST` (`ID_USER_GUEST`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `NUMBER_PLATE_FK`
+    FOREIGN KEY (`VEHICLES_NUMBER_PLATE`)
+    REFERENCES `sistema`.`VEHICLES` (`NUMBER_PLATE`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
