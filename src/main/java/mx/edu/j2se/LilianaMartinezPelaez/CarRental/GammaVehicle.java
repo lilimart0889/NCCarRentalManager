4904badb9bbc3208efebe0d383236ddc476c0d39
Lili <lilimart0808@gmail.com>
2021-05-02 19:10:43 -0500

    Class "GammaVehicle" is finished.

---
+++ src/main/java/mx/edu/j2se/LilianaMartinezPelaez/CarRental/GammaVehicle.java
@@ -0,0 +1,33 @@
+package mx.edu.j2se.LilianaMartinezPelaez.CarRental;
+
+/* AUTHOR: LILI MP
+ * NC TRAINEE PROGRAM
+ * PROJECT: CAR RENTAL
+ */
+
+public class GammaVehicle {
+
+    private String idGammaVehicle;
+    //private String description;
+
+    //*********************************
+    // CONSTRUCTORS                   *
+    // ********************************
+
+    public GammaVehicle(String idGammaVehicle) {
+        this.idGammaVehicle = idGammaVehicle;
+    }
+
+    //*********************************
+    // METHODS                        *
+    // ********************************
+
+    public String getIdGammaVehicle() {
+        return idGammaVehicle;
+    }
+    public void setIdGammaVehicle(String idGammaVehicle) {
+        this.idGammaVehicle = idGammaVehicle;
+    }
+
+
+}
