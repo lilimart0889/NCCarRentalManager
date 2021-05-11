8d1afc32067b5a64965f70871c8b92b1bf17ff78
Lili <lilimart0808@gmail.com>
2021-05-11 14:22:25 -0500

    Package "Springboot": Spring boot application

---
+++ src/main/java/mx/edu/j2se/LilianaMartinezPelaez/CarRental/springboot/CarRentalTBWSystem.java
@@ -0,0 +1,18 @@
+package mx.edu.j2se.LilianaMartinezPelaez.CarRental.springboot;
+
+/*AUTHOR: LILI MP
+ * NC TRAINEE PROGRAM
+ * PROJECT: CAR RENTAL "TRIPS BY WHEELS"
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalTBWSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalTBWSystem.class, args);
+
+    }
+}
