c19d00fcaccc8ff24c52f745d6bfbac09299a6a8
Lili <lilimart0808@gmail.com>
2021-05-11 14:19:57 -0500

    Package "Service": Interfaces and their implementations.

---
+++ src/main/java/mx/edu/j2se/LilianaMartinezPelaez/CarRental/service/GammaVehicleService.java
@@ -0,0 +1,16 @@
+package mx.edu.j2se.LilianaMartinezPelaez.CarRental.service;
+
+/*AUTHOR: LILI MP
+ * NC TRAINEE PROGRAM
+ * PROJECT: CAR RENTAL "TRIPS BY WHEELS"
+ */
+
+import mx.edu.j2se.LilianaMartinezPelaez.CarRental.model.GammaVehicle;
+
+import java.util.List;
+
+public interface GammaVehicleService {
+
+    List<GammaVehicle> findAllGamma();
+
+}
